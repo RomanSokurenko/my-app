dea4e7cae29d5884b6b1e713c95ba7b49fb9cbd7
RomanSokurenko <sokurenko.roman82@gmail.com>
2021-09-06 20:44:39 +0300

    save остановился на api запросе
    https://youtu.be/qdjQuJy30PE

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Employees from "components/Employees";
+import Jobs from "components/Jobs";
+import React from "react";
+
+function App() {
+   return (
+      <div>
+         <Jobs />
+         <Employees />
+      </div>
+   );
+}
+
+export default App;
---
+++ src/modules/api/api.js
@@ -0,0 +1,10 @@
+const Base_URL = 'https://5f7998dbe402340016f9321f.mockapi.io/jobs';
+
+class Api {
+   constructor(baseUrl, endpoind) {
+      this.baseUrl = baseUrl;
+      this.endpoind = endpoind;
+
+   }
+   async fetch() { };
+}
